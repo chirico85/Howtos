8fb19c4ff91e3edccc62eb65c719247fe0e97a7c
chirico85 <juliocgg@gmail.com>
2017-03-30 16:05:35 +0200

    Add files via upload
    updated

--- search_columns.py
+++ search_columns.py
@@ -8,6 +8,10 @@ Created on Wed Mar 29 12:25:57 2017
 import urllib.request
 from lxml import html
 import requests
+import re
+#from parse import *
+from urllib.parse import urlparse
+from urllib.parse import search
 
 import os
 cwd = os.getcwd()
@@ -17,6 +21,7 @@ file = 'file://'+cwd+'/myfile5.html'
 #f = codecs.open('myfile5.html', 'r', 'utf-8')
 
 myfile5 = urllib.request.urlopen(file).read()
+myfile5 = myfile5.decode("utf-8") 
 #page = requests.get('myfile5.html')
 tree = html.fromstring(myfile5)
 
@@ -37,5 +42,18 @@ for n in data_correct:
         data_correct[count] = n+data_correct[count+1]
         #data_correct.append(n + data[data.index(n)+1])
         del data_correct[count+1]
-        
- 
+
+count = -1
+for n in data_correct:
+    count += 1
+    data_correct[count].strip()
+
+#top = re.search("top:([0-9]+)px.", myfile5, re.MULTILINE)
+top = re.findall("div style.*top:([0-9]+)px.", myfile5)
+left = re.findall("div style.*left:([0-9]+)px.", myfile5)
+top.group(0)
+
+
+mo = re.search("([0-9]+).*: (.*)", "Customer number: 232454, Date: February 12, 2011")
+mo.group()
+
